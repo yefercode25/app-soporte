ce640db47dcd64d5d46e02f017a40edcb99b507b
Yeferson Zaque <zaquecamilo@gmail.com>
2024-02-05 20:18:08 -0500

    Segunda corrección en la hora para ajustar a la zona horaria de colombia

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -1,15 +1,9 @@
 export const fechaFormateada = (fecha: string) => {
   const parseDate = new Date(fecha);
-  const options = { weekday: 'long', day: '2-digit', month: 'long' };
-  return parseDate.toLocaleDateString('es-ES', options as any);
+  return parseDate.toLocaleDateString('es-CO', { weekday: 'long', day: '2-digit', month: 'long' });
 };
 
 export const horaFormateada = (fecha: string) => {
   const parseDate = new Date(fecha);
-  const hour = parseDate.getHours();
-  const minute = parseDate.getMinutes();
-  const ampm = hour >= 12 ? 'PM' : 'AM';
-  const formattedHour = hour % 12 === 0 ? 12 : hour % 12;
-  const formattedMinute = minute < 10 ? `0${minute}` : minute;
-  return `${formattedHour}:${formattedMinute} ${ampm}`;
+  return parseDate.toLocaleTimeString('es-CO', { hour: '2-digit', minute: '2-digit' });
 };
