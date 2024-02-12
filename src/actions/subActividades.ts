5d569c306bfa6d823b82d9850ceaea351467cc12
Yeferson Zaque <zaquecamilo@gmail.com>
2024-02-11 21:54:02 -0500

    Se ha implementado el completado o pendiente de las tareas de las actividades

--- src/actions/subActividades.ts
+++ src/actions/subActividades.ts
@@ -8,6 +8,9 @@ export const listarSubActivities = async (idActivity: string) => {
     const subActivities = await prisma.subActivity.findMany({
       where: {
         activityId: idActivity
+      },
+      orderBy: {
+        isCompleted: 'asc',
       }
     });
 
@@ -57,6 +60,41 @@ export const crearSubActivity = async (data: IGestionarSubTarea) => {
   } catch (error) {
     return {
       message: 'Se ha producido un error al crear la sub actividad, intente nuevamente.',
+      statusCode: 500,
+      statusText: 'INTERNAL_SERVER_ERROR'
+    }
+  }
+};
+
+export const toggleSubActivity = async (idSubActivity: string) => {
+  try {
+    const findSubActivity = await prisma.subActivity.findUnique({ where: { id: idSubActivity }});
+    if (!findSubActivity) {
+      return {
+        data: null,
+        message: 'La sub actividad que cambió de estado no existe',
+        statusCode: 404,
+        statusText: 'NOT_FOUND'
+      }
+    }
+
+    const subActivity = await prisma.subActivity.update({
+      where: { id: idSubActivity },
+      data: {
+        isCompleted: !findSubActivity.isCompleted
+      }
+    });
+
+    return {
+      message: 'Sub actividad completada',
+      data: subActivity,
+      statusCode: 200,
+      statusText: 'OK'
+    }
+  } catch (error) {
+    return {
+      data: null,
+      message: 'Se ha producido un error al completar la sub actividad, intente nuevamente.',
       statusCode: 500,
       statusText: 'INTERNAL_SERVER_ERROR'
     }
--- src/components/list/SubActivyItem.tsx
+++ src/components/list/SubActivyItem.tsx
@@ -1,12 +1,28 @@
 'use client';
 
+import { toggleSubActivity } from "@/actions";
 import { SubActivity } from "@/types"
+import { toastAlert } from "@/utils/toastAlert";
 import { fechaFormateada, horaFormateada } from "@/utils/dates";
 import { useState } from "react";
 import { IoCalendarOutline } from "react-icons/io5";
 
 export const SubActivyItem = ({ activityId, createdAt, id, isCompleted, title }: SubActivity) => {
   const [completed, setCompleted] = useState(isCompleted);
+
+  const toggleCompleted = async () => {
+    try {
+      const updatedSubActivity = await toggleSubActivity(id);
+      if (updatedSubActivity.statusCode === 200) {
+        setCompleted(!completed);
+        toastAlert({ title: 'Estado actualizado', description: `La tarea ha sido marcada como ${!completed ? 'completada' : 'pendiente'}.`, tipo: 'success'});
+      } else {
+        toastAlert({ title: 'Error estado', description: updatedSubActivity.message, tipo: 'error'});
+      }
+    } catch (error) {
+      toastAlert({ title: 'Error estado', description: 'Se ha producido un error al cambiar el estado de la sub actividad, intente nuevamente.', tipo: 'error'});
+    }
+  };
 
   return (
     <div className="flex items-center gap-1 p-2 border-b border-gray-500 last-of-type:border-b-0">
@@ -16,14 +32,14 @@ export const SubActivyItem = ({ activityId, createdAt, id, isCompleted, title }:
             type="checkbox"
             className="form-checkbox h-5 w-5 text-gray-600"
             checked={completed}
-            onChange={() => setCompleted(!completed)}
+            onChange={() => toggleCompleted()}
           />
           <span className="ml-2 text-gray-700"></span>
         </label>
       </div>
       <div>
-        <h3 className="font-semibold">{title}</h3>
-        <p className="text-gray-500 text-sm flex items-center gap-1"><IoCalendarOutline /> <span>{fechaFormateada(createdAt.toISOString())}, {horaFormateada(createdAt.toISOString())}</span></p>
+        <h3 className={`font-semibold ${completed ? 'text-green-800 line-through' : ''}`}>{title}</h3>
+        <p className="text-gray-500 text-sm flex items-center gap-1"><IoCalendarOutline /> <span className={completed ? 'line-through' : ''}>{fechaFormateada(createdAt.toISOString())}, {horaFormateada(createdAt.toISOString())}</span></p>
       </div>
     </div>
   )
